503234cd21ddc8367be807d9ad94657ab39c85c2
Nicole Fisher <ncspence4@gmail.com>
2016-01-17 14:55:20 -0800

    Added calculate average controller

--- averageController.js
+++ averageController.js
@@ -1,6 +1,21 @@
-app.controller('averageController', function () {
-    var self=this;
-    self.
-        });
+app.controller('averageController', function ($scope) {
+        var self = this;
+        self.calculateAverage = function () {
+            var sum = 0;
+            var count = 0;
+            for (var i = 0; i < sum; i++) {
+                count += $scope.students[i].grade;
+                count++;
+            }
+            var average = sum / count;
+            var avgGrade = Math.round(average)
+            if (students.length === 0) {
+                avgGrade = 0;
+            }
+            return avgGrade;
+        }
+
+    });
+
 
 
